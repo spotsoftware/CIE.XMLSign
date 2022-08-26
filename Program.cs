3498631d014651e70f35913620a18a81e0d6e1c0
Marco Maroni <marco.maroni@spot.it>
2022-08-26 09:47:35 +0200

    Paremetrizzati i paremetri per la scelta dello store del certificato, migliorati l'help per la linea di comando.

--- Program.cs
+++ Program.cs
@@ -5,20 +5,87 @@ using System.Xml;
 
 try
 {
-    if (args.Length <= 1)
+    if (args.Length <= 3 || (args.Length == 1 && args[0] == "/?"))
     {
-        Console.WriteLine("Uso CIE.XMLSign [file xml da firmare] [nome certificato]");
-        return;
+        Console.WriteLine("Uso CIE.XMLSign [file xml da firmare] [nome certificato] [nome store certificato] [scope certificato]");
+        Console.WriteLine(" [file xml da firmare]: percorso completo del file xml da firmare");
+        Console.WriteLine(" [nome certificato]: attributo CN del 'subject name' del certificato con cui cercarlo nello store");
+        Console.WriteLine(" [store certificato]: \"AddressBook\"");
+        Console.WriteLine("                      \"AuthRoot\"");
+        Console.WriteLine("                      \"CertificateAuthority\"");
+        Console.WriteLine("                      \"My\"");
+        Console.WriteLine("                      \"Root\"");
+        Console.WriteLine("                      \"TrustedPeople\"");
+        Console.WriteLine("                      \"TrustedPublisher\"");
+        Console.WriteLine(" [scope certificato]: \"LocalMachine\"");
+        Console.WriteLine("                      \"CurrentUser\"");
+        Console.WriteLine("");
+        Console.WriteLine("Esempio");
+        Console.WriteLine("Es. CIE.XMLSign \"test.xml\" \"test.dominio.it\" \"My\" \"CurrentUser\"");
+        return 0;
     }
 
-    var certificate = GetCertificateFromStore(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindBySubjectName, args[1], false);
-    RSA rsaKey = certificate.GetRSAPrivateKey();
+    StoreLocation sl;
+    switch (args[3])
+    {
+        case "LocalMachine":
+            sl = StoreLocation.LocalMachine;
+            break;
+        case "CurrentUser":
+            sl = StoreLocation.CurrentUser;
+            break;
+        default:
+            Console.WriteLine("ERRORE: Scope del certificato non valido");
+            return 0;
+    }
+
+    StoreName st;
+    switch (args[2])
+    {
+        case "My":
+            st = StoreName.My;
+            break;
+        case "AddressBook":
+            st = StoreName.AddressBook;
+            break;
+        case "AuthRoot":
+            st = StoreName.AuthRoot;
+            break;
+        case "CertificateAuthority":
+            st = StoreName.CertificateAuthority;
+            break;
+        case "Root":
+            st = StoreName.Root;
+            break;
+        case "TrustedPeople":
+            st = StoreName.TrustedPeople;
+            break;
+        case "TrustedPublisher":
+            st = StoreName.TrustedPublisher;
+            break;
+        default:
+            Console.WriteLine("ERRORE: Nome store certificato non valido");
+            return 0;
+    }
+
+    var certificate = GetCertificateFromStore(sl, st, X509FindType.FindBySubjectName, args[1], false);
+    if (certificate == null)
+    {
+        throw new Exception("ERRORE: il certificato non è stato trovato");
+    }
+
+    RSA? rsaKey = certificate.GetRSAPrivateKey();
+    if (rsaKey == null)
+    {
+        throw new Exception("ERRORE: errore durante il reperimento della chiave privata del certificato");
+    }
 
     // Create a new XML document.
-    XmlDocument xmlDoc = new XmlDocument();
-
-    // Load an XML file into the XmlDocument object.
-    xmlDoc.PreserveWhitespace = true;
+    XmlDocument xmlDoc = new()
+    {
+        // Load an XML file into the XmlDocument object.
+        PreserveWhitespace = true
+    };
     xmlDoc.Load(args[0]);
 
     // Sign the XML document.
@@ -31,8 +98,10 @@ try
 }
 catch (Exception e)
 {
-    Console.WriteLine(e.Message);
+    Console.WriteLine($"ERRORE: {e.Message}");
 }
+
+return -1;
 
 static void SignXml(XmlDocument xmlDoc, RSA rsaKey, X509Certificate2 certificate)
 {
@@ -43,23 +112,26 @@ static void SignXml(XmlDocument xmlDoc, RSA rsaKey, X509Certificate2 certificate
         throw new ArgumentException(nameof(rsaKey));
 
     // Create a SignedXml object.
-    SignedXml signedXml = new SignedXml(xmlDoc);
-
-    // Add the key to the SignedXml document.
-    signedXml.SigningKey = rsaKey;
+    SignedXml signedXml = new(xmlDoc)
+    {
+        // Add the key to the SignedXml document.
+        SigningKey = rsaKey
+    };
 
     // Create a reference to be signed.
-    Reference reference = new Reference();
-    reference.Uri = "";
+    Reference reference = new Reference
+    {
+        Uri = ""
+    };
 
     // Add an enveloped transformation to the reference.
-    XmlDsigEnvelopedSignatureTransform env = new XmlDsigEnvelopedSignatureTransform();
+    XmlDsigEnvelopedSignatureTransform env = new();
     reference.AddTransform(env);
 
     // Add the reference to the SignedXml object.
     signedXml.AddReference(reference);
 
-    KeyInfo keyInfo = new KeyInfo();
+    KeyInfo keyInfo = new();
     keyInfo.AddClause(new KeyInfoX509Data(certificate));
     signedXml.KeyInfo = keyInfo;
 
@@ -73,16 +145,16 @@ static void SignXml(XmlDocument xmlDoc, RSA rsaKey, X509Certificate2 certificate
 
     // Append the element to the XML document.
     //xmlDoc.DocumentElement.AppendChild(xmlDoc.ImportNode(xmlDigitalSignature, true));
-    xmlDoc.DocumentElement.InsertBefore(xmlDigitalSignature, xmlDoc.DocumentElement.ChildNodes[0]);
+    _ = xmlDoc.DocumentElement.InsertBefore(xmlDigitalSignature, xmlDoc.DocumentElement.ChildNodes[0]);
 }
 
-static X509Certificate2 GetCertificateFromStore(StoreLocation storeLocation, StoreName storeName, X509FindType findType, object findValue, bool validOnly)
+static X509Certificate2? GetCertificateFromStore(StoreLocation storeLocation, StoreName storeName, X509FindType findType, string findValue, bool validOnly)
 {
     using X509Store store = new X509Store(storeName, storeLocation);
     store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
-    X509Certificate2Collection coll = store.Certificates.Find(findType, findValue.ToString(), validOnly);
+    X509Certificate2Collection coll = store.Certificates.Find(findType, findValue, validOnly);
 
-    X509Certificate2 certificate = null;
+    X509Certificate2? certificate = null;
     if (coll.Count > 0)
     {
         certificate = coll[0];
